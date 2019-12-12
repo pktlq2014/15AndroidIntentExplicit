ccd6414d18ae320a5b4099f0354dd3c1d1a830a9
unknown <nguyenvanty10987654321000@gmail.com>
2019-12-12 19:23:09 +0700

    15AndroidIntentExplicit

---
+++ app/src/main/java/com/example/a15androidintentexplicit/Main2Activity.java
@@ -0,0 +1,29 @@
+package com.example.a15androidintentexplicit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity
+{
+    Button button2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        button2 = findViewById(R.id.butTon2);
+        button2.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(Main2Activity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
